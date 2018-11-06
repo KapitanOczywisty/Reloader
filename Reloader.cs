658c8bb2d4fea8a4918762a140c7ee730f1e41cb
Kapitan Oczywisty <44417092+KapitanOczywisty@users.noreply.github.com>
2018-11-06 22:06:34 +0100

    New methods will point to original dll
    This allows to preserve instances and fields values, but you cannot
    create new methods, types nor fields when reloading.

--- Reloader.cs
+++ Reloader.cs
@@ -5,6 +5,8 @@ using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using Harmony;
+using System.Reflection.Emit;
 
 namespace Reloader
 {
@@ -18,10 +20,16 @@ namespace Reloader
 	{
 
 		Dictionary<string, MethodInfo> reloadableMethods = new Dictionary<string, MethodInfo>();
+		static Dictionary<string, Assembly> originalAssemblies = new Dictionary<string, Assembly>();
 		ModContentPack content;
+		HarmonyInstance harmony;
+		HarmonyMethod transpiler;
 
 		public Reloader(ModContentPack content) : base(content)
 		{
+			harmony = HarmonyInstance.Create("Reloader.Unstable");
+			transpiler = new HarmonyMethod(AccessTools.Method(typeof(Reloader), "TranslationTranspiler"));
+
 			var modDirName = Path.GetDirectoryName(content.RootDir).ToLower();
 
 			this.content = content;
@@ -51,7 +59,7 @@ namespace Reloader
 		void CacheExstingMethods()
 		{
 
-            AppDomain.CurrentDomain.GetAssemblies()
+			AppDomain.CurrentDomain.GetAssemblies()
 				.Where(assembly =>
 				{
 					var name = assembly.FullName;
@@ -63,6 +71,9 @@ namespace Reloader
 				.ForEach(assembly =>
 				{
 					Log.Warning("Reloader: analyzing " + assembly.FullName);
+					var name = assembly.FullName;
+					name = name.Split(',')[0];
+					originalAssemblies[name] = assembly;
 					assembly.GetTypes().ToList()
 						.ForEach(type => type.GetMethods(allBindings)
 							.ToList()
@@ -72,13 +83,13 @@ namespace Reloader
 								if (method.TryGetAttribute(out attr))
 								{
 
-                                    var key = method.DeclaringType.FullName + "." + method.Name;
-								    if (method.IsGenericMethodDefinition)
-								    {
-								        Log.Error($"Reloader: Cannot reload generic method definition {key} - skipping");
-								        return;
-								    }
-                                    reloadableMethods[key] = method;
+									var key = method.DeclaringType.FullName + "." + method.Name;
+									if (method.IsGenericMethodDefinition)
+									{
+										Log.Error($"Reloader: Cannot reload generic method definition {key} - skipping");
+										return;
+									}
+									reloadableMethods[key] = method;
 									var methodType = method.DeclaringType;
 									Log.Warning("Reloader: found reloadable method " + key);
 								}
@@ -87,10 +98,108 @@ namespace Reloader
 				});
 		}
 
+		public static T FindOldMember<T>(T[] elements, T target) {
+			return elements.ToList().Find(c => c.ToString() == target.ToString());
+		}
+
+		public static bool ReplaceMember<T>(T found, CodeInstruction code) {
+			if(found != null)
+			{
+				//code = new CodeInstruction(code.opcode, found);
+				code.operand = found;
+				return true;
+			}
+			else
+			{
+				Log.Error("Reloader: MemeberNotFound!");
+				return false;
+			}
+		}
+
+		static IEnumerable<CodeInstruction> TranslationTranspiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var codes = new List<CodeInstruction>(instructions);
+
+			Log.Warning("Transpiler on! "+codes.Count);
+
+			for (int i = 0; i < codes.Count; i++)
+			{
+				Log.Message(codes[i] +"", true);
+
+				var member = codes[i].operand as MemberInfo;
+				if (member == null)
+					continue;
+
+				var name = member.Module.Assembly.FullName.Split(',')[0];
+
+				if (!originalAssemblies.ContainsKey(name))
+					continue;
+
+				Assembly assembly = originalAssemblies[name];
+
+				//Log.Warning("MemberInfo / ToString:" + member.Module.ToString() + " / AssemblyName: " + name + " / MemberType" + member.MemberType.ToString(), true);
+
+				var type = member.MemberType;
+				
+				// Type
+				if (type == MemberTypes.TypeInfo)
+				{
+					Type found = assembly.GetType((codes[i].operand as Type).FullName);
+					ReplaceMember(found, codes[i]);
+					continue;
+				}
+
+				var parent = assembly.GetType(member.DeclaringType.FullName);
+
+				// MethodInfo
+				if (type == MemberTypes.Method)
+				{
+					var found = FindOldMember(parent.GetMethods(), codes[i].operand as MethodInfo);
+					ReplaceMember(found, codes[i]);
+				}
+				// FieldInfo
+				else if (type == MemberTypes.Field)
+				{
+					var found = FindOldMember(parent.GetFields(), codes[i].operand as FieldInfo);
+					ReplaceMember(found, codes[i]);
+				}
+				// PropertyInfo
+				else if (type == MemberTypes.Property)
+				{
+					var found = FindOldMember(parent.GetProperties(), codes[i].operand as PropertyInfo);
+					ReplaceMember(found, codes[i]);
+				}
+				// EventInfo
+				else if (type == MemberTypes.Event)
+				{
+					var found = FindOldMember(parent.GetEvents(), codes[i].operand as EventInfo);
+					ReplaceMember(found, codes[i]);
+				}
+				// ConstructorInfo
+				else if (type == MemberTypes.Constructor)
+				{
+					var found = FindOldMember(parent.GetConstructors(), codes[i].operand as ConstructorInfo);
+					ReplaceMember(found, codes[i]);
+				}
+
+				else
+				{
+					Log.Error("Unknown member "+ type);
+				}
+
+
+
+
+
+			}
+			
+			return codes.AsEnumerable();
+		}
+
 		void LoadPath(string path)
 		{
 
-            var assembly = Assembly.Load(File.ReadAllBytes(path));
+			var assembly = Assembly.Load(File.ReadAllBytes(path));
 			assembly.GetTypes().ToList()
 				.ForEach(type => type.GetMethods(allBindings)
 					.ToList()
@@ -106,18 +215,20 @@ namespace Reloader
 							if (originalMethod != null)
 							{
 								var originalCodeStart = Memory.GetMethodStart(originalMethod, out Exception ex1);
-							    if (ex1 != null) {
-							        Log.Warning($"Reloader: exception getting original method: {ex1.Message}");
-							        return;
-							    }
+								if (ex1 != null)
+								{
+									Log.Warning($"Reloader: exception getting original method: {ex1.Message}");
+									return;
+								}
 
-							    var newCodeStart = Memory.GetMethodStart(newMethod, out Exception ex2);
-							    if (ex2 != null)
-							    {
-							        Log.Warning($"Reloader: exception getting new method: {ex2.Message}");
-							        return;
-							    }
+								var newCodeStart = Memory.GetMethodStart(newMethod, out Exception ex2);
+								if (ex2 != null)
+								{
+									Log.Warning($"Reloader: exception getting new method: {ex2.Message}");
+									return;
+								}
 								Memory.WriteJump(originalCodeStart, newCodeStart);
+								harmony.Patch(newMethod, null, null, transpiler);
 							}
 							else
 								Log.Warning("Reloader: original missing");
